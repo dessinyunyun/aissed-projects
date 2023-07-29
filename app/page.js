4e98eb1e9d62330cc3f192ac3b53e331010df9d8
Dessinyunyun <dessinyunyun@gmail.com>
2023-07-29 11:45:23 +0800

    aboutme checkpoint

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Hero from "@/components/main/Hero/Hero";
 import Aboutme from "@/components/main/about me/Aboutme";
@@ -7,15 +7,28 @@ import Projects from "@/components/main/projects/Projects";
 import Skills from "@/components/main/skills/Skills";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    // Simulasi waktu pemuatan untuk "Hero" (Anda dapat mengganti setTimeout dengan pemrosesan data atau pemanggilan API jika diperlukan)
+    const fakeLoadingTime = 2000; // 2 detik
+    setTimeout(() => {
+      setLoading(false);
+    }, fakeLoadingTime);
+  }, []);
   return (
     <div className={`w-full flex flex-col items-center`}>
-      <Hero />
-
-      <Aboutme />
-
-      <Projects />
-
-      <Skills />
+      {loading ? (
+        // Tampilkan loading screen ketika loading masih true
+        <div className="text-grey">Loading...</div>
+      ) : (
+        // Tampilkan konten utama ketika loading menjadi false
+        <>
+          <Hero />
+          <Aboutme />
+          <Projects />
+          <Skills />
+        </>
+      )}
     </div>
   );
 }
